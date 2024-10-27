09a883556bfb37162fdc03f234e134c9688eee61
MaineroJuani <mainerojuani@gmail.com>
2024-10-27 19:25:35 -0300

    Todas las compus cargadas

---
+++ recursos/js/productos.js
@@ -0,0 +1,139 @@
+const productos = {
+    computadoras: [
+        {
+        marca: "apple",
+        modelo: "Macbook Air M2",
+        precio: 1900000,
+        descripcion: "",
+        categoria: "Diseño - Arquitectura",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "apple",
+        modelo: "MacBook Pro M2",
+        precio: 2214999,
+        descripcion: "",
+        categoria: "Arquitectura",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "apple",
+        modelo: "Macbook Pro M1",
+        precio: 1750000,
+        descripcion: "",
+        categoria: "Diseño",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "hp",
+        modelo: "HP Victus Core i5 8gb",
+        precio: 1394000,
+        descripcion: "",
+        categoria: "Diseño",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "hp",
+        modelo: "HP Victus Core i5 16gb",
+        precio: 1360000,
+        descripcion: "",
+        categoria: "Diseño",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "hp",
+        modelo: "HP Victus Core i7",
+        precio: 1800000,
+        descripcion: "",
+        categoria: "Gamer",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "lenovo",
+        modelo: "Lenovo idePad3 slim",
+        precio: 1000000,
+        descripcion: "",
+        categoria: "Economica",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "lenovo",
+        modelo: "Lenovo LOQ Core",
+        precio: 1500000,
+        descripcion: "",
+        categoria: "Gamer",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "lenovo",
+        modelo: "Lenovo E16 R7",
+        precio: 1969000,
+        descripcion: "",
+        categoria: "Gamer - Diseño",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "msi",
+        modelo: "MSI GE78HX 13VG RAIDER",
+        precio: 4725299,
+        descripcion: "",
+        categoria: "Gamer - Arquitectura - Diseño",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "msi",
+        modelo: "MSI CREATOR M16 B13VE-642AR",
+        precio: 2911999,
+        descripcion: "",
+        categoria: "Gamer - Arquitectura - Diseño",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "msi",
+        modelo: "MSI GF63 THIN i5",
+        precio: 1469999,
+        descripcion: "",
+        categoria: "Gamer - Arquitectura - Diseño",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "asus",
+        modelo: "Notebook Asus X515ea-ej3968 Fhd I3",
+        precio: 769299,
+        descripcion: "",
+        categoria: "Economica",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "asus",
+        modelo: "Notebook Gamer ASUS TUF Gaming F15 FX506LHB-HN324W",
+        precio: 1501999,
+        descripcion: "",
+        categoria: "Gamer - Diseño",
+        imagen: "",
+        detalle_imagen: ""
+        },
+        {
+        marca: "asus",
+        modelo: "Notebook Asus TUF A16 Ryzen 7 16GB 512GB Radeon RX770",
+        precio: 2159999,
+        descripcion: "",
+        categoria: "Arquitectura",
+        imagen: "",
+        detalle_imagen: ""
+        }
+    ]    
+}
